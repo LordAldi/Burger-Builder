0dc856fe0e004240fcc739247c671d91b8710c3e
LordAldi <aldianugra09@gmail.com>
2020-07-03 06:59:12 +0700

    add auto login

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,5 +1,6 @@
 import * as actionTypes from './actionTypes'
 import axios from 'axios'
+import logo from '../../components/Logo/Logo'
 
 export const authStart = ()=>{
     return{
@@ -24,6 +25,9 @@ export const authFail = (error)=>{
 }
 
 export const logout = () => {
+    localStorage.removeItem('token')
+    localStorage.removeItem('expirationDate')
+    localStorage.removeItem('userId')
     return {
         type: actionTypes.AUTH_LOGOUT
     }
@@ -52,6 +56,10 @@ export const auth = (email, password, isSignup) => {
         }
         axios.post(url, authData)
         .then(response  => {
+            const expirationDate = new Date(new Date().getTime() + response.data.expiresIn * 1000)
+            localStorage.setItem('token', response.data.idToken)
+            localStorage.setItem('expirationDate', expirationDate)
+            localStorage.setItem('userId', response.data.localId)
             dispatch(authSucess(response.data.idToken, response.data.localId))
             dispatch(checkAuthTimeout(response.data.expiresIn))
         })
@@ -65,5 +73,25 @@ export const setAuthRedirectPath = (path) => {
     return{
         type: actionTypes.SET_AUTH_REDIRECT_PATH,
         path: path
+    }
+}
+
+
+export const authCheckState = () => {
+    return dispatch => {
+        const token = localStorage.getItem('token')
+        if (!token){
+            dispatch(logout())
+        } else {
+            const expirationDate = new Date(localStorage.getItem('expirationDate'))
+            if (expirationDate <= new Date()){
+                dispatch(logout())
+            } else{
+                const userId = localStorage.getItem('userId')
+                dispatch(authSucess(token, userId))
+                dispatch(checkAuthTimeout((expirationDate.getTime() - new Date().getTime())/ 1000))
+            }
+            
+        }
     }
 }
